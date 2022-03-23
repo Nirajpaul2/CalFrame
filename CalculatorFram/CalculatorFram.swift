ee7b1c6654074ea60fb8c5c4a5a39907962e5679
Purplle <niraj.p@purplle.com>
2022-03-23 16:30:13 +0530

    initial com

---
+++ CalculatorFram/CalculatorFram.swift
@@ -0,0 +1,15 @@
+//
+//  CalculatorFram.swift
+//  CalculatorFram
+//
+//  Created by Purplle on 23/03/22.
+//
+
+import Foundation
+
+public class Calculator {
+    
+   public static func addTwoNo(a: Int, b: Int) -> Int {
+        return a+b
+    }
+}
---
+++ build/CalculatorFram.framework-iphonesimulator.xcarchive/Products/Library/Frameworks/CalculatorFram.framework/Headers/CalculatorFram.h
@@ -0,0 +1,18 @@
+//
+//  CalculatorFram.h
+//  CalculatorFram
+//
+//  Created by Purplle on 23/03/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CalculatorFram.
+FOUNDATION_EXPORT double CalculatorFramVersionNumber;
+
+//! Project version string for CalculatorFram.
+FOUNDATION_EXPORT const unsigned char CalculatorFramVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CalculatorFram/PublicHeader.h>
+
+
